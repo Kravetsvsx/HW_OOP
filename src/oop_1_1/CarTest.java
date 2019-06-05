6a6e005933215a7b26caa3d1decd0ab51210d9de
Kravetsvsx <kravetsvsx@gmail.com>
2019-06-05 17:12:44 +0300

    Commit31

--- src/oop_1_1/CarTest.java
+++ src/oop_1_1/CarTest.java
@@ -2,9 +2,12 @@ package oop_1_1;
 
 public class CarTest {
     public static void main(String[] args) {
+
         Car opel = new Car(10);
+
         opel.fillCar(50);
         System.out.println(opel.carMovement(200));
+
         opel.fillCar(50);
         System.out.println(opel.carMovement(200));
         System.out.println(opel);
